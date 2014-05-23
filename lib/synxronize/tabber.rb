71a78fb7fb78d62f912063df9b2ff7eb04d743e1
Mark Larsen <larse503@gmail.com>
2014-05-23 14:58:02 -0700

    tabber for putsing out with tabs. specs.

---
+++ lib/synxronize/tabber.rb
@@ -0,0 +1,34 @@
+module Synxronize
+  class Tabber
+
+    @@tabbing = ""
+
+    class << self
+      def increase(n=1)
+        @@tabbing += (a_single_tab * n)
+      end
+
+      def decrease(n=1)
+        @@tabbing.sub!((a_single_tab * n), "")
+      end
+
+      def current
+        @@tabbing.length / a_single_tab.length
+      end
+
+      def reset
+        @@tabbing = ""
+      end
+
+      def puts(str="")
+        Kernel.puts @@tabbing + str
+      end
+
+      def a_single_tab
+        return "  "
+      end
+      private :a_single_tab
+
+    end
+  end
+end
