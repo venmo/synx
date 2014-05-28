aefdddc9c21357b5aadb1566d7f64920261cf71b
Mark Larsen <larse503@gmail.com>
2014-05-27 20:48:10 -0700

    change to cleaner implementation

--- lib/synx/tabber.rb
+++ lib/synx/tabber.rb
@@ -1,27 +1,28 @@
 module Synx
   class Tabber
 
-    @@tabbing = ""
+    @@tabbing = 0
 
     class << self
       def increase(n=1)
-        @@tabbing += (a_single_tab * n)
+        @@tabbing += n
       end
 
       def decrease(n=1)
-        @@tabbing.sub!((a_single_tab * n), "")
+       @@tabbing -= n
+       @@tabbing = 0 if @@tabbing < 0
       end
 
       def current
-        @@tabbing.length / a_single_tab.length
+        @@tabbing
       end
 
       def reset
-        @@tabbing = ""
+        @@tabbing = 0
       end
 
       def puts(str="")
-        Kernel.puts @@tabbing + str.to_s
+        Kernel.puts (a_single_tab * @@tabbing) + str.to_s
       end
 
       def a_single_tab
